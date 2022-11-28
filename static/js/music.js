043f9f1bd2e44c926a4b435cc0742c144a811ae2
Jaehyeonee <anjaehyeon0412@gmail.com>
2022-11-28 16:30:33 +0900

    음악 부분 수정중

--- static/js/music.js
+++ static/js/music.js
@@ -1,23 +1,32 @@
 
 const audio_volume = document.querySelector("#cmusic");
+const audioSet = document.querySelector("#volume-control");
 
 
-function volume_control(){
-    audio_volume.volume = 5;
+function volume_control(con){
+    audioVolume.addEventListener("change", function(e) {
+        audio.volume = this.value/10;
+    })
+
+
+    audio_volume.volume = 0.5;
     // 집중도 척도 
 
     // 집중도 낮을 때 -> 볼륨 up
-    if(con_data <= 0.97 ){  
-        audio_volume.volume = 9;
+    if(con <= 1.97 ){  
+        audio_volume.volume = 0.9;
     }
     // 집중도 높을 때 -> 볼륨 down
-    else if (con_data >= 1.35){
-        audio_volume.volume = 1;
+    else if (con >= 2.35){
+        audio_volume.volume = 0.1;
     }
     else{
-        audio_volume = 0;
+        audio_volume = 0.0;
     }
     
 }
+
+
+
 
 

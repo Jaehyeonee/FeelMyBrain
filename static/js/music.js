fc5becf7f0a6d1224e0afb2606a91a2325f7ad55
Jibin-Song <sjbpsh1@sookmyung.ac.kr>
2022-11-28 13:52:36 +0900

    알람 울리도록 구현, 알람 횟수, 순집중도 시간 변수 생성해서 저장함

--- static/js/music.js
+++ static/js/music.js
@@ -2,17 +2,26 @@
 const audio_volume = document.querySelector("#cmusic");
 
 
-function volume_control(){
-    audio_volume.volume = 5;
+function volume_control(con){
+    music_cnt = music_cnt + 1;
+    
+    alert('볼륨 조절'+ 'music_cnt' + music_cnt);
+
+    // audio_volume.volume = 5;
     // 집중도 척도 
+    
 
     // 집중도 낮을 때 -> 볼륨 up
-    if(con_data <= 0.97 ){  
+    if(con <= 1.97 ){  
         audio_volume.volume = 9;
+        music_sum = music_sum + 9;
+        alert('볼륨 up');
     }
     // 집중도 높을 때 -> 볼륨 down
-    else if (con_data >= 1.35){
+    else if (con >= 2.35){
         audio_volume.volume = 1;
+        music_sum = music_sum + 1;
+        alert('볼륨 down');
     }
     else{
         audio_volume = 0;
@@ -21,3 +30,23 @@ function volume_control(){
 }
 
 
+
+// $(document).ready(function(){
+//     if(show){
+//         setInterval(function(){
+//             $.ajax({
+//                 url: '/music',
+//                 type: 'GET',
+//                 success: function(data){
+//                     alert('/music에서 success') 
+    
+//                     volume_control(data)
+//                     alert('music 조절 데이터를 얻었습니다.') 
+//                 },
+//                 error: alert('실패: music 음악 조절 데이터 얻지 못함')
+//             })
+//         }, 1000);
+//     }
+    
+    
+//   });
